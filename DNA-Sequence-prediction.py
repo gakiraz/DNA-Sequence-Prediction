ba65cadb2777ca2e3191e94f7c5c1d37da0b5879
Gabriel Akira <gabriel.akira2198@gmail.com>
2025-08-05 23:38:10 -0300

    data formating

--- DNA-Sequence-prediction.py
+++ DNA-Sequence-prediction.py
@@ -1,8 +1,33 @@
 # %%
 import pandas as pd
 import matplotlib as plt
+import nltk
+import numpy as np
+
 # %%
+
 df = pd.read_csv("archive/train.csv")
 
 # %%
+
 df.head()
+df.info()
+df.isna().sum()
+
+# %%
+new_columns = df["Description"].str.split(" ", expand=True)
+
+# %%
+df["prefix"] = df["Description"].str.extract(r'^([A-Za-z]+)')
+# %%
+df = pd.concat([df, new_columns], axis=1)
+# %%
+for col in df.columns:
+    if pd.api.types.is_string_dtype(df[col]):
+                df[col] = df[col].str.replace(",", "")
+# %%
+df.columns
+# %%
+for col in df.columns:
+    if pd.api.types.is_string_dtype(df[col]):
+        print(df[col].value_counts().head(20))
