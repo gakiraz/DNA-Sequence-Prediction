a1e8917ba05998e10aa9e777ed1c3d66d9147c2f
Gabriel Akira <gabriel.akira2198@gmail.com>
2025-08-07 02:10:44 -0300

    training some shit

--- DNA-Sequence-prediction.py
+++ DNA-Sequence-prediction.py
@@ -1,11 +1,10 @@
 # %%
 import pandas as pd
 import matplotlib as plt
-import nltk
 import numpy as np
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, roc_auc_score, auc
 # %%
-
 df = pd.read_csv("archive/train.csv")
 
 # %%
@@ -13,21 +12,24 @@ df = pd.read_csv("archive/train.csv")
 df.head()
 df.info()
 df.isna().sum()
-
+df.isnull().sum()
 # %%
 new_columns = df["Description"].str.split(" ", expand=True)
 
 # %%
 df["prefix"] = df["Description"].str.extract(r'^([A-Za-z]+)')
+
 # %%
 df = pd.concat([df, new_columns], axis=1)
+
 # %%
 for col in df.columns:
     if pd.api.types.is_string_dtype(df[col]):
                 df[col] = df[col].str.replace(",", "")
+
 # %%
-df.columns
+features = df[['Symbol','GeneType','GeneGroupMethod','prefix',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]]
+target = df['NucleotideSequence']
+
 # %%
-for col in df.columns:
-    if pd.api.types.is_string_dtype(df[col]):
-        print(df[col].value_counts().head(20))
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
